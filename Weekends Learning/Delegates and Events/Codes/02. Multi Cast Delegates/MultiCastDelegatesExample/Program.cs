70040e5901d46d38f1bccc3f94dd125aa8278a49
Shivam Vishwakarma <arvind.972176@gmail.com>
2023-02-11 21:34:18 +0530

    11-02-2023

---
+++ Weekends Learning/Delegates and Events/Codes/02. Multi Cast Delegates/MultiCastDelegatesExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace MultiCastDelegatesExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create object of Sample
+            Sample s = new Sample();
+
+            //create reference variable of MyDelegateType
+            MyDelegateType myDelegate;
+
+            //add ref of first target method
+            // Internally it will create an object of delegate  in that delegate obejct the referenct add method will stored and then the reference of the delegate object will be stored in the reference variable called myDelegate.
+            myDelegate = s.Add;
+
+            //add ref of second target method
+            myDelegate += s.Multiply;
+
+            //invoke both target methods; first Add method; and then Multiply method
+            myDelegate.Invoke(40, 10);
+
+             
+        }
+    }
+}
---
+++ Weekends Learning/Delegates and Events/Codes/03. Events/ClassLibrary1/Publisher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    //delegate type
+    public delegate void MyDelegateType(int a, int b);
+
+    //publisher
+    public class Publisher
+    {
+        //private delegate
+        private MyDelegateType myDelegate;
+
+        //step 1: create event
+        public event MyDelegateType myEvent
+        {
+            add
+            {
+                myDelegate += value;
+            }
+            remove
+            {
+                myDelegate -= value;
+            }
+        }
+
+        public void RaiseEvent(int a, int b)
+        {
+            //step 2: raise event
+            // atleast one or more methods have been subscribe to the event then only we have raise the event
+            if (this.myDelegate != null)
+            {
+                this.myDelegate(a, b);
+            }
+        }
+    }
+}
