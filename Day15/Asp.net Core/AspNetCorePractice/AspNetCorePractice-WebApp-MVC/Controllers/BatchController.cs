0ab519834fa23fdca53aa3dd75b6a5b729f4d8a7
Shivam Vishwakarma <arvind.972176@gmail.com>
2023-02-17 23:45:03 +0530

    17-02-2023

---
+++ Day15/Asp.net Core/AspNetCorePractice/AspNetCorePractice-WebApp-MVC/Controllers/BatchController.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCorePractice_WebApp_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AspNetCorePractice_WebApp_MVC.Controllers
+{
+    public class BatchController : Controller
+    {
+        private readonly AspNetCorePracticeDbContext _context;
+        public BatchController(AspNetCorePracticeDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Batch> batchList = _context.Batches.ToList();
+            return View(batchList);
+        }
+    }
+}
