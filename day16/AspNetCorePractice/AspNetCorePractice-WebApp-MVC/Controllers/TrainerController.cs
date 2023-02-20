518d9e071130314badb70e316e345b20a938ab02
Shivam Vishwakarma <arvind.972176@gmail.com>
2023-02-20 21:12:31 +0530

    20-02-2023

---
+++ day16/AspNetCorePractice/AspNetCorePractice-WebApp-MVC/Controllers/TrainerController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetCorePractice_WebApp_MVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AspNetCorePractice_WebApp_MVC.Controllers
+{
+    public class TrainerController : Controller
+    {
+        private readonly AspNetCorePracticeDbContext dbContext;
+        public TrainerController(AspNetCorePracticeDbContext dbContext) {
+            this.dbContext = dbContext;        
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Trainer>  trainers = dbContext.Trainers.ToList();
+
+
+            return View(trainers);  
+        }
+        [HttpGet]
+        public IActionResult GetTrainerDetail(int trainerID)
+        {
+            Trainer trainer = dbContext.Trainers.Find(trainerID);
+            return View(trainer);
+        }
+        [HttpGet]
+        public IActionResult DeleteTrainer (int trainerId)
+        {
+            Trainer trainer = dbContext.Trainers.Find(trainerId);
+            dbContext.Trainers.Remove(trainer);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult AddNewTrainer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNewTrainer(Trainer trainer)
+        {
+            dbContext.Trainers.Add(trainer);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ day16/AspNetCorePractice/AspNetCorePractice-WebApp-MVC/Models/Batch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCorePractice_WebApp_MVC.Models
+{
+    public class Batch
+    {
+        [Key]
+        public int BatchNo { get; set; }
+        public string BatchName { get; set; }
+        public DateTime StartSate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int TrainerId { get; set; }
+        public Trainer Trainer { get; set;}
+        public int CourseId { get; set; }
+        public Course Course { get; set;}
+
+    }
+}
