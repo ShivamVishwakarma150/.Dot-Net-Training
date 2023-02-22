8b9d99cd6e1b101ed7335b6db856371953775ae2
Shivam Vishwakarma <arvind.972176@gmail.com>
2023-02-22 16:17:50 +0530

    22-02-2023

---
+++ day18/Covid-Management-System-WebAPI/Covid-Management-System-WebAPI/Controllers/NurseController.cs
@@ -0,0 +1,45 @@
+﻿using Covid_Management_System_WebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Covid_Management_System_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NurseController : ControllerBase
+    {
+        private readonly COVID_DBContext _context;
+        public NurseController(COVID_DBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetNurse")]
+        public List<Nurse> GetNurse()
+        {
+            List<Nurse> nurses = _context.Nurses.ToList();
+            return nurses;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Nurse nurse)
+        {
+            _context.Nurses.Add(nurse);
+            _context.SaveChanges();
+            return Created("Patient Added", nurse);
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            Nurse nurses = _context.Nurses.Find(id);
+            nurses.IsActive = false;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
